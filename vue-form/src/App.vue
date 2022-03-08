c9f145932b653137a8959089ff6ee070aaa3edb9
정종찬 <jeongjongchan@jeongjongchan-ui-MacBookAir.local>
2022-03-08 13:40:01 +0900

    [0308] 완강

---
+++ vue-form/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id:</label>
+      <input type="text" id="username" v-model="username" />
+    </div>
+    <div>
+      <label for="password">pw:</label>
+      <input type="text" id="password" v-model="password" />
+    </div>
+    <button type="submit">login</button>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm: function () {
+      // event.preventDefault();
+      console.log(this.username, this.password);
+      const url = "https://jsonplaceholder.typicode.com/users";
+      const data = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(url, data)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
